2962720b98ec9e8a06d10e7689c75793339ecc4f
Dean <deaneffi1414@gmail.com>
2026-02-08 09:48:53 +0200

    add next specific data-scroll-behavior to html tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html
+      lang="en"
+      className="scroll-smooth"
+      data-scroll-behavior="smooth"
+    >
       <body
         className={`${inter.className}  antialiased bg-neutral-50 text-neutral-950 font-light `}
       >
